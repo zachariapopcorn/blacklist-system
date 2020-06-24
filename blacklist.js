aac479fd9b499c0d003cc8c6538272c37d18c12f
zachariapopcorn <52849920+zachariapopcorn@users.noreply.github.com>
2020-06-24 09:59:41 -0700

    Update blacklist.js

--- blacklist.js
+++ blacklist.js
@@ -1,14 +1,14 @@
 const db = require('../db.js');
 
 exports.run = async (client, message, args) => {
-    if(message.author.id != "465362236693807115") {
+    if(message.author.id != "DISCORD ID HERE") {
         return message.channel.send("I'm sorry, but you don't have the correct permissions to execute this command!");
     }
     let data
     try {
         data = await db.get("blacklisted");
     } catch (err) {
-        console.log('There was an error: ' + err);
+        return message.channel.send('There was an error: ' + err);
     }
     if(data == null) {
         data = [];
@@ -25,16 +25,4 @@ exports.run = async (client, message, args) => {
         return message.channel.send("There was an error while adding this user to the blacklist!");
     }
     return message.channel.send("Successfully added user to blacklist database!");
-}
-
-exports.help = (client, message, args) => {
-    message.channel.send({embed: {
-        color: 7948427,
-        description: `**Blacklist**\n`
-        + `\`${client.config.prefix}blacklist <user>\` - Blacklists a user from using the bot, sadly only the owner of the bot can use this.`,
-        author: {
-            name: message.author.tag,
-            icon_url: message.author.displayAvatarURL()
-        }
-    }});
 }
